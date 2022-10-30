9eee1bea0e1b4d150b98aafeecc3e6696826cff0
emma Vazquez <emmavazquez700@gmail.com>
2022-10-30 16:16:29 -0500

    adding comments

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
+// npm packages 
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
-
+// asking app to listen on port 3000
+// express installament 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+// connecting our routes 
 require('./routes/routes')(app);
 
+// funciton is being used to listen and bind connections
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 });  
